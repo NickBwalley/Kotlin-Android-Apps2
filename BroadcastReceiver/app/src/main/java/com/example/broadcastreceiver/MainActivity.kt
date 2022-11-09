e0ecb58d139db0bfa31070893418d9a56ddce1ba
NickBwalley <nickbwalley@gmail.com>
2022-11-09 15:12:10 +0300

    Broadcast Receiver

--- BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
+++ BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.broadcastreceiver
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var intent = Intent(this, CustomReceiver::class.java)
+        intent.action = "com.example.CUSTOM_INTENT" // indicate which type of intent to respond to... this is a custom intent
+        sendBroadcast(intent)
     }
 }
