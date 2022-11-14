cba18e7f3b1eacad5ee7c37b21719574d8dff472
NickBwalley <nickbwalley@gmail.com>
2022-11-14 21:02:07 +0300

    precommit

---
+++ CustomBroadcastReceiver/app/src/main/java/com/example/custombroadcastreceiver/CustomReceiver.kt
@@ -0,0 +1,11 @@
+package com.example.custombroadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class CustomReceiver : BroadcastReceiver(){
+    override fun onReceive(context: Context, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
