b4a987b6dc704c3cd9d799277287035bef4047a4
NickBwalley <nickbwalley@gmail.com>
2022-11-14 21:42:24 +0300

    custom broadcast receivers

--- CustomBroadcastReceiver/app/src/main/java/com/example/custombroadcastreceiver/CustomReceiver.kt
+++ CustomBroadcastReceiver/app/src/main/java/com/example/custombroadcastreceiver/CustomReceiver.kt
@@ -3,9 +3,10 @@ package com.example.custombroadcastreceiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class CustomReceiver : BroadcastReceiver(){
     override fun onReceive(context: Context, intent: Intent?) {
-        TODO("Not yet implemented")
+        Toast.makeText(context, "Broadcast event has been received.", Toast.LENGTH_LONG).show()
     }
 }
