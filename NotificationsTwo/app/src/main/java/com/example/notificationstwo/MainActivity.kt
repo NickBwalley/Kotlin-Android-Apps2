d7a43fd6181a740158ef5ecd0af33992a3ca3b1a
NickBwalley <nickbwalley@gmail.com>
2022-11-14 21:34:34 +0300

    Notifications Two

--- NotificationsTwo/app/src/main/java/com/example/notificationstwo/MainActivity.kt
+++ NotificationsTwo/app/src/main/java/com/example/notificationstwo/MainActivity.kt
@@ -1,82 +1,38 @@
 package com.example.notificationstwo
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
-import android.widget.Button
-import android.widget.RemoteViews
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-
 class MainActivity : AppCompatActivity() {
-
-    // declaring variables
-    lateinit var notificationManager: NotificationManager
     lateinit var notificationChannel: NotificationChannel
+    lateinit var notificationManager: NotificationManager
     lateinit var builder: Notification.Builder
-    private val channelId = "i.apps.notifications"
-    private val description = "Test notification"
-
+    private val channelId = "12345"
+    private val description = "Test Notification"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // accessing button
-        val btn = findViewById<Button>(R.id.btn)
-
-        // it is a class to notify the user of events that happen.
-        // This is how you tell the user that something has happened in the
-        // background.
-        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        // onClick listener for the button
-        btn.setOnClickListener {
-
-            // pendingIntent is an intent for future use i.e after
-            // the notification is clicked, this intent will come into action
-            val intent = Intent(this, afterNotification::class.java)
-
-            // FLAG_UPDATE_CURRENT specifies that if a previous
-            // PendingIntent already exists, then the current one
-            // will update it with the latest intent
-            // 0 is the request code, using it later with the
-            // same method again will get back the same pending
-            // intent for future reference
-            // intent passed here is to our afterNotification class
-            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-            // RemoteViews are used to use the content of
-            // some different layout apart from the current activity layout
-            val contentView = RemoteViews(packageName, R.layout.activity_after_notification)
-
-            // checking if android version is greater than oreo(API 26) or not
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                notificationChannel = NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
-                notificationChannel.enableLights(true)
-                notificationChannel.lightColor = Color.GREEN
-                notificationChannel.enableVibration(false)
-                notificationManager.createNotificationChannel(notificationChannel)
-
-                builder = Notification.Builder(this, channelId)
-                    .setContent(contentView)
-                    .setSmallIcon(R.drawable.ic_launcher_background)
-                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
-                    .setContentIntent(pendingIntent)
-            } else {
-
-                builder = Notification.Builder(this)
-                    .setContent(contentView)
-                    .setSmallIcon(R.drawable.ic_launcher_background)
-                    .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
-                    .setContentIntent(pendingIntent)
-            }
-            notificationManager.notify(1234, builder.build())
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as
+                NotificationManager
+    }
+    fun btnNotify(view: View) {
+        val intent = Intent(this, LauncherActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(channelId, description, NotificationManager .IMPORTANCE_HIGH)
+            notificationChannel.lightColor = Color.BLUE
+            notificationChannel.enableVibration(true)
+            notificationManager.createNotificationChannel(notificationChannel)
+            builder = Notification.Builder(this, channelId).setContentTitle("FULLTIME DECEMBER EXAMS " +
+                    "2022").setContentText("Soma bro!. Exams zitakufinya!. Read!").setSmallIcon(R.drawable .ic_brightness).setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable
+                .ic_launcher_background)).setContentIntent(pendingIntent)
         }
+        notificationManager.notify(12345, builder.build())
     }
 }
