3f847a6e4d57e4809a0b50d36510a6938c54be19
NickBwalley <nickbwalley@gmail.com>
2022-11-15 10:21:10 +0300

    custom broadcast receiver

--- CustomBroadcastReceiver/app/src/main/java/com/example/custombroadcastreceiver/CustomReceiver.kt
+++ CustomBroadcastReceiver/app/src/main/java/com/example/custombroadcastreceiver/CustomReceiver.kt
@@ -3,10 +3,27 @@ package com.example.custombroadcastreceiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
+import android.provider.Settings
 import android.widget.Toast
 
 class CustomReceiver : BroadcastReceiver(){
     override fun onReceive(context: Context, intent: Intent?) {
         Toast.makeText(context, "Broadcast event has been received.", Toast.LENGTH_LONG).show()
+        // declaring object of MediaPlayer
+        lateinit var player: MediaPlayer
+
+        // creating a media player which
+        // will play the audio of Default
+        // ringtone in android device
+        player = MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI)
+
+        // providing the boolean
+        // value as true to play
+        // the audio on loop
+        player.setLooping(true)
+
+        // starting the process
+        player.start()
     }
 }
