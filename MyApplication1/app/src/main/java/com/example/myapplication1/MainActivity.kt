21f3d724dcb477521c8c0be4832e271e4e475a02
NickBwalley <nickbwalley@gmail.com>
2022-11-17 21:49:19 +0300

    Application1- assignment

--- MyApplication1/app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ MyApplication1/app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -14,11 +15,13 @@ class MainActivity : AppCompatActivity() {
         var submitButton : Button = findViewById(R.id.submitBtn)
 
         submitButton.setOnClickListener(){
+            var intent = Intent(this, SecondActivity::class.java)
             val text = "Hello world!"
             val duration = Toast.LENGTH_SHORT
 
             val Toast =Toast.makeText(this, text, duration)
             Toast.show()
+            startActivity(intent)
         }
     }
 
