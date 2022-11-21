45b23cea9ac87327a6298e8ca2ba25cdc999cec2
NickBwalley <nickbwalley@gmail.com>
2022-11-21 16:28:44 +0300

    precommit

--- RoomDB/app/src/main/java/com/example/roomdb/Student.kt
+++ RoomDB/app/src/main/java/com/example/roomdb/Student.kt
@@ -1,3 +1,9 @@
 package com.example.roomdb
 
-data class Student()
+@Entity(tableName = "student_table") // must provide this annotation
+data class Student(
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name="id") val id: Long=0,
+    @ColumnInfo(name="name") val userName: String,
+    @ColumnInfo(name="YearOfBirth") val userYOB: String,
+    @ColumnInfo(name="email") val userEmail: String
+)
--- RoomDB/app/src/main/java/com/example/roomdb/StudentDao.kt
+++ RoomDB/app/src/main/java/com/example/roomdb/StudentDao.kt
@@ -1,4 +1,17 @@
 package com.example.roomdb
 
 interface StudentDao {
+    
+    @Insert //provide this annotation before your function name to insert data into dataBase
+    fun insertNewStudent(student: Student)
+    
+    @Update // provide this annotation before your function name to update existing data in the database
+    fun updateStudentData(student: Student)// try this functionality out on your own
+    
+    @Delete //provide this annotation before your function name to delete existing data from database
+    fun deleteExistingStudent(student: Student) // try this functionality on your own
+    
+    // use the query annotation to provide a custom SQL query that is verifiable at run-time
+    @Query("SELECT * FROM student_table")
+    fun getRecords() : List<Student>
 }
